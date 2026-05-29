6ba100c750dffd45f2adba4109d2a920cf2b3658
e2611 <e2611@s.akashi.ac.jp>
2026-05-29 17:59:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
-let Presentdate=new Date()
-let tuki=Presentdate.getMonth()
-let hi=Presentdate.getDate()
-let youbi=Presentdate.getDay()
-tuki += 1;
+let Presentdate=new Date();
+let tuki=Presentdate.getMonth();
+let hi=Presentdate.getDate();
+let youbi=Presentdate.getDay();
+let tuki-device=tuki+1;
 console.log(tuki+'月'+hi+'日'+youbi);
-document.querySelector('#month-device').textContent=`${tuki}月`;
+document.querySelector('#month-device').textContent=`${tuki-device}月`;
