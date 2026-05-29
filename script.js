e95d09e8d427aebad51c3dc2468244eaaa29fc4a
e2611 <e2611@s.akashi.ac.jp>
2026-05-29 18:33:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ let youbi=Presentdate.getDay();
 let tukidevice=tuki+1;
 console.log(tukidevice+'月'+hi+'日'+youbi);
 document.getElementById('month-display').textContent = `${tukidevice}月`;
+let youbidevice;
+if(youbi==1){
+  youbidevice="月曜日";
+}
+if(youbi==2){
+  youbidevice="火曜日";
+}
+if(youbi==3){
+  youbidevice="水曜日";
+}
+if(youbi==4){
+  youbidevice="木曜日";
+}
+if(youbi==5){
+  youbidevice="金曜日";
+}
+if(youbi==6){
+  youbidevice="土曜日";
+}
+if(youbi==0){
+  youbidevice="日曜日";
+}
