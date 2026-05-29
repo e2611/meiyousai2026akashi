df82a79c24bbf7518a3e9d89fe4658ed5c25a38e
e2611 <e2611@s.akashi.ac.jp>
2026-05-29 18:13:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ let hi=Presentdate.getDate();
 let youbi=Presentdate.getDay();
 let tukidevice=tuki+1;
 console.log(tukidevice+'月'+hi+'日'+youbi);
-document.getElementById('month-display').textContent='${tukidevice}月';
+document.getElementById('month-display').textContent = `${tukidevice}月`;
