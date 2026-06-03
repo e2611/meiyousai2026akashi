6869526b5d3c146de2e64fadb8b97b75188dd63a
e2611 <e2611@s.akashi.ac.jp>
2026-06-03 16:53:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,37 @@ document.getElementById('month-display').textContent = `${tukidevice}月`;
 let youbidevice;
 if(youbi==1){
   youbidevice="月曜日";
+  let karidate=new Date();
   document.getElementById('monday').textContent=`${hi}(月)`;
+  karidate.setDate(Presentdate.getDate()+1);
+  hi=karidate.getDate()
+  document.getElementById('tuesday').textContent=`${hi}(火)`;
+  karidate.setDate(Presentdate.getDate()+2);
+  hi=karidate.getDate()
+  document.getElementById('wednesday').textContent=`${hi}(水)`;
+  karidate.setDate(Presentdate.getDate()+3);
+  hi=karidate.getDate()
+  document.getElementById('thursday').textContent=`${hi}(木)`;
+  karidate.setDate(Presentdate.getDate()+5);
+  hi=karidate.getDate()
+  document.getElementById('friday').textContent=`${hi}(金)`;
 }
 if(youbi==2){
   youbidevice="火曜日";
+  let karidate=new Date();
   document.getElementById('tuesday').textContent=`${hi}(火)`;
+  karidate.setDate(Presentdate.getDate()+1);
+  hi=karidate.getDate()
+  document.getElementById('wednesday').textContent=`${hi}(水)`;
+  karidate.setDate(Presentdate.getDate()+2);
+  hi=karidate.getDate()
+  document.getElementById('thursday').textContent=`${hi}(木)`;
+  karidate.setDate(Presentdate.getDate()+3);
+  hi=karidate.getDate()
+  document.getElementById('friday').textContent=`${hi}(金)`;
+  karidate.setDate(Presentdate.getDate()-1);
+  hi=karidate.getDate()
+  document.getElementById('monday').textContent=`${hi}(月)`;
 }
 if(youbi==3){
   youbidevice="水曜日";
@@ -35,11 +61,37 @@ if(youbi==3){
 }
 if(youbi==4){
   youbidevice="木曜日";
+  let karidate=new Date();
   document.getElementById('thursday').textContent=`${hi}(木)`;
+  karidate.setDate(Presentdate.getDate()+1);
+  hi=karidate.getDate()
+  document.getElementById('friday').textContent=`${hi}(金)`;
+  karidate.setDate(Presentdate.getDate()-1);
+  hi=karidate.getDate()
+  document.getElementById('wednesday').textContent=`${hi}(水)`;
+  karidate.setDate(Presentdate.getDate()-2);
+  hi=karidate.getDate()
+  document.getElementById('thursday').textContent=`${hi}(木)`;
+  karidate.setDate(Presentdate.getDate()-3);
+  hi=karidate.getDate()
+  document.getElementById('monday').textContent=`${hi}(月)`;
 }
 if(youbi==5){
   youbidevice="金曜日";
+  let karidate=new Date();
   document.getElementById('friday').textContent=`${hi}(金)`;
+  karidate.setDate(Presentdate.getDate()-1);
+  hi=karidate.getDate()
+  document.getElementById('thursday').textContent=`${hi}(木)`;
+  karidate.setDate(Presentdate.getDate()-2);
+  hi=karidate.getDate()
+  document.getElementById('wednesday').textContent=`${hi}(水)`;
+  karidate.setDate(Presentdate.getDate()-3);
+  hi=karidate.getDate()
+  document.getElementById('tuesday').textContent=`${hi}(火)`;
+  karidate.setDate(Presentdate.getDate()-4);
+  hi=karidate.getDate()
+  document.getElementById('monday').textContent=`${hi}(月)`;
 }
 if(youbi==6){
   youbidevice="土曜日";
