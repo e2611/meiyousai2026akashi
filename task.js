4357ff921e41ec63cdb6b3ea06f4861c153fb86d
e2611 <e2611@akashi.kosen-ac.jp>
2026-06-16 18:02:34 +0900

    a

--- task.js
+++ task.js
@@ -3,54 +3,37 @@ const inputDate = document.querySelector('.date');
 const addBtn = document.querySelector('.add-btn');
 const taskList = document.querySelector('.task-list');
 
-//ページがロードされたら処理を実行する
+
 window.addEventListener('load', () => {
  displayTasks();
  TaskListBtnEvent();
 });
-
-//追加ボタンを押したら処理を実行する
 addBtn.addEventListener('click', () => {
- //タスクの入力フォームが空の場合
  if(!inputForm.value) {
   const errorMsg = document.querySelector('.error-msg');
-  //errorMsgにshowクラスを追加する
   errorMsg.classList.add('show');
   return;
  }
- //タスクidを設定する
  let taskId = setTaskId();
- //タスクのデータのオブジェクトを作成する
  const task = {
   id: taskId,
   content: inputForm.value,
   date: inputDate.value ? formattedDate(inputDate.value) : null,
  }
- //タスクリスト(ulタグ)にタスクを追加する 
  taskList.innerHTML += createTaskElement(task); 
- //完了ボタン、削除ボタンのイベント
  TaskListBtnEvent();
- //ローカルストレージにタスクデータを保存する
  saveLocalStorage(task);
- //入力フォームをリセットする
  inputForm.value = '';
  inputDate.value = '';
 });
-
-//タスクの入力フォームでキーが離されたときに処理を実行する
 inputForm.addEventListener('keyup', () => {
   const errorMsg = document.querySelector('.error-msg');
-  //errorMsgにshowクラスがある場合
   if(errorMsg.classList.contains('show')) {
-    //タスクの入力フォームが空がどうか
     if(inputForm.value !== '') {
-      //errorMsgのshowクラスを取り除く
       errorMsg.classList.remove('show');
     }
   }
 });
-
-//タスクを表示するためのHTMLタグを作成する
 const createTaskElement = (task) => {
     return `<li class="task-item" data-task-id="${task.id}">
     ${task.content}
@@ -63,28 +46,22 @@ const createTaskElement = (task) => {
     </div>
    </li>`;
 }
- 
-//ローカルストレージにタスクを保存する
 const saveLocalStorage = (task) => { 
-  //ローカルストレージに保存されているタスクデータを取得する
   const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  //tasksに入力したタスクデータを追加する
   tasks.push(task);
-  //ローカルストレージにtasksを保存する
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-//ローカルストレージにタスクがある場合は表示する
+
 const displayTasks = () => {
- //taskuList(ulタグ)をリセットする
  taskList.innerHTML = '';
- //ローカルストレージに保存されているタスクデータを取得する
+
   const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  //ローカルストレージにタスクのデータが1つ以上ある場合
+
   if(tasks.length !== 0) {
-    //タスクを1つずつ取り出して処理をする
+
     tasks.forEach((task) => {
-      //taskList(ulタグ)にタスクを追加する
+  
       taskList.innerHTML += createTaskElement(task);
     });
   }
